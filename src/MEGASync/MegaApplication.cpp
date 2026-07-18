d68bbb14c3bb605427e1ca7553e14f688924f308
Haowen Liu <liu.haowen.andy@gmail.com>
2026-07-18 16:01:17 -0400

    Use real home in snap

--- src/MEGASync/MegaApplication.cpp
+++ src/MEGASync/MegaApplication.cpp
@@ -3235,6 +3235,10 @@ QString MegaApplication::getDefaultUploadPath()
     {
         defaultFolderPath = paths.at(0);
     }
+    if (qEnvironmentVariableIsSet("SNAP_REAL_HOME"))
+    {
+        defaultFolderPath = qEnvironmentVariable("SNAP_REAL_HOME");
+    }
     return defaultFolderPath;
 }
 
